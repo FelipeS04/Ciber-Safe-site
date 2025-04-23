c19d6f85d27dacda34101fb53530b9d0f91012b6
Jeziel Monteiro <luiz.m.jeziel@gmail.com>
2025-04-23 19:34:45 -0300

    Update serializers.py

--- learning_logs/serializers.py
+++ learning_logs/serializers.py
@@ -4,14 +4,17 @@ from .models import Topic, Entry, Noticia
 class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
-        fields = '__all__'
+        fields = ['id', 'text', 'date_added']
+        read_only_fields = ['date_added']
 
 class EntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Entry
-        fields = '__all__'
+        fields = ['id', 'topic', 'text', 'date_added']
+        read_only_fields = ['date_added']
 
 class NoticiaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Noticia
-        fields = '__all__'
+        fields = ['id', 'titulo', 'conteudo', 'data_publicacao', 'categoria']
+        read_only_fields = ['data_publicacao']
